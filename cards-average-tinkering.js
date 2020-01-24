cad63b55c1bf56ad2a78ae6512e032272cbb27f3
Kevin Kim <kevinfkim@gmail.com>
2020-01-24 04:33:05 +0000

    I think a WHILE loop works better than a FOR loop as we're not iterating anything, just doing something over and over.

--- cards-average-tinkering.js
+++ cards-average-tinkering.js
@@ -8,11 +8,12 @@ Let's try 10,000 times and see the average.
 */
 
 let currentCard;        //draw a shape
+let i = 1;              //start 10,000 at 1
 let counter = 0;        //how many draws before the shape is SPADES
 let totalCounter = 0;   //totalling through 10,000 tries
 let average;            //our goal number
 
-for (i = 1; i < 10000; i ++) { //lets do it 10,000 times
+while (i <= 10000) { //lets do it 10,000 times
   while (currentCard != "spade") { //draw cards until the shape matches SPADES
     currentCard = cards[Math.floor(Math.random() * 4)]; //randomized number/card
     counter ++; //how many pulls before we got a match?
@@ -20,6 +21,7 @@ for (i = 1; i < 10000; i ++) { //lets do it 10,000 times
   totalCounter += counter; //add the number of pulls for all tries
   currentCard = null; //reset and try again
   counter = 0; //reset and try again
+  i ++; //increment by 1, and try again
 }
 average = totalCounter/10000;
 console.log(`It takes an average of ${average.toFixed(2)} pulls it get SPADES from 4 shapes.`);
