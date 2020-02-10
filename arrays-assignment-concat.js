91ecbd43040496ee320045f79a599844df3e8bf3
Kevin Kim <kevinfkim@gmail.com>
2020-02-10 04:57:01 +0000

    Concatenating arrays, using .push().

---
+++ arrays-assignment-concat.js
@@ -0,0 +1,15 @@
+function concat(arr1, arr2) {
+  let concatArr = [];
+  for (let i = 0; i <= arr1.length - 1; i++) {
+    concatArr.push(arr1[i]);
+  }
+  for (let i = 0; i <= arr2.length - 1; i++) {
+    concatArr.push(arr2[i]);
+  }
+  return concatArr;
+}
+
+console.log(concat([1, 2, 3], [4, 5, 6]));
+console.log(concat([0, 3, 1], [9, 7, 2]));
+console.log(concat([], [9, 7, 2]));
+console.log(concat([5, 10], []));
