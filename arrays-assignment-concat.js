422cd8a3a4a8571f0c38fbc230a482e3153d3c94
Kevin Kim <kevinfkim@gmail.com>
2020-02-10 05:00:28 +0000

    made the for iterations a bit more easier to read, made it simpler.

--- arrays-assignment-concat.js
+++ arrays-assignment-concat.js
@@ -1,9 +1,9 @@
 function concat(arr1, arr2) {
   let concatArr = [];
-  for (let i = 0; i <= arr1.length - 1; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     concatArr.push(arr1[i]);
   }
-  for (let i = 0; i <= arr2.length - 1; i++) {
+  for (let i = 0; i < arr2.length; i++) {
     concatArr.push(arr2[i]);
   }
   return concatArr;
