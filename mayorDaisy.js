75e56ec6ca755a1f515aa0bfa376f5e4cabf1add
Kevin Kim <kevinfkim@gmail.com>
2020-03-26 01:48:09 +0000

    practicing pushing to objects

---
+++ mayorDaisy.js
@@ -0,0 +1,28 @@
+/*
+
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40
+  },
+  {
+    time: 1568329821632,
+    speed: 42
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+];
+
+const speed = 38;
+
+*/
+
+const carPassing = function(cars, speed) {
+  // send the appropriate info to the array
+  cars.push({ time: Date.now(), speed: speed });
+  return cars;
+};
+
+// carPassing(cars, speed);
